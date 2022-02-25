4e4ece48526db508b506e80b075df713661b1a98
ramck007 <98875517+ramck007@users.noreply.github.com>
2022-02-25 22:30:45 +0530

    Add files via upload

---
+++ GofGProg3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.Scanner;
+public class GofGProg3 {
+	//Write a program to reverse the array
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String s= sc.nextLine();
+		for(int i=s.length()-1;i>=0;i--) {
+			System.out.print("The Reversed String is "+s.charAt(i));
+		}
+	}
+
+}
